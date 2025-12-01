85dd87c2eb3c72ca3b82740bd26818208dabfc45
danyyil <danyyil.shykerynets@estudiante.uam.es>
2025-12-01 20:40:53 +0100

    Day One completed

---
+++ 01/01_p2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  FILE *input_f, *output_f;
+  char buf[64];
+  int num, count = 0;
+  int dial = 50;
+
+  input_f = fopen("01_input.dat", "r");
+  if (input_f == NULL)
+    return EXIT_FAILURE;
+
+  output_f = fopen("01_output.dat", "w");
+  if (output_f == NULL)
+    return EXIT_FAILURE;
+
+  while (fscanf(input_f, "%s", buf) == 1) {
+    /* Dato leido */
+    num = atoi(buf + 1);
+
+    /* Tipo de rotacion */
+    if (buf[0] == 'L') {
+      if (dial == 0)
+        dial = 100;
+      dial -= num;
+      while (dial <= 0) {
+        dial += 100;
+        count++;
+      }
+      if (dial == 100)
+        dial = 0;
+    } else if (buf[0] == 'R') {
+      count += (dial + num) / 100;
+      dial = (dial + num) % 100;
+    } else {
+      fclose(input_f);
+      fclose(output_f);
+      return EXIT_FAILURE;
+    }
+
+    fprintf(output_f, "Dial after \"%s\": %d | count = %d\n", buf, dial, count);
+  }
+
+  fprintf(stdout, "The result is: %d\n", count);
+
+  fclose(input_f);
+  fclose(output_f);
+
+  return EXIT_SUCCESS;
+}
